246e8a89a21059c22768aeb00f5dd7f18ad5d89d
jeremy-parker-yang <72048688+jeremy-parker-yang@users.noreply.github.com>
2021-05-21 17:38:23 -0400

    basic classes

---
+++ Tri.java
@@ -0,0 +1,73 @@
+/**
+ * Triangle defined as one point and two vector edges. Assumes right-handed
+ * coordinate system.
+ * 
+ * @author Jeremy Parker Yang
+ *
+ */
+public class Tri {
+	Vector3 a; // point a
+	Vector3 e1; // edge 1
+	Vector3 e2; // edge 1
+
+	// TODO: local coords
+	Vector3 n; // normal
+	Vector3 t1; // tangent 1
+	Vector3 t2; // tangent 2
+
+	/**
+	 * Construct triangle from 3 vertices
+	 * 
+	 * @param a Cartesian coords vertex a
+	 * @param b Cartesian coords vertex b
+	 * @param c Cartesian coords vertex c
+	 */
+	public Tri(Vector3 a, Vector3 b, Vector3 c) {
+		// defines triangle
+		this.a = a;
+		e1 = b.sub(a);
+		e2 = c.sub(a);
+
+		// defines local coord system
+		n = e1.cross(e2).norm();
+		// t1
+		// t2
+	}
+
+	/**
+	 * Moller-Trumbore algorithm for ray-triangle intersection
+	 * 
+	 * @param o origin of the ray
+	 * @param d direction of ray
+	 * @return true if there is a hit
+	 * @return hit point of intersection
+	 */
+	public boolean MTint(Vector3 o, Vector3 d, Vector3 hit) {
+
+		// back facing triangles do not intersect
+		if (d.dot(n) < 0)
+			return false;
+
+		Vector3 t = o.sub(a);
+		Vector3 p = d.cross(e2);
+		Vector3 q = t.cross(e1);
+
+		double k = 1d / p.dot(e1);
+		double u = k * p.dot(t);
+
+		// check intersection
+		if (u < 0 || u > 1)
+			return false;
+
+		double v = k * q.dot(d);
+
+		// check intersection
+		if (v < 0 || u + v > 1)
+			return false;
+
+		// calc point of intersection
+		hit = o.add(d.mul(k * q.dot(e2)));
+
+		return true;
+	}
+}
---
+++ Vector3.java
@@ -0,0 +1,62 @@
+/**
+ * Simple 3D vector math class
+ * 
+ * @author Jeremy Parker Yang
+ *
+ */
+public class Vector3 {
+	private final double x;
+	private final double y;
+	private final double z;
+
+	/**
+	 * Construct vector
+	 */
+	public Vector3(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * Vector addition
+	 */
+	Vector3 add(Vector3 a) {
+		return new Vector3(this.x + a.x, this.y + a.y, this.z + a.z);
+	}
+	
+	/**
+	 * Vector subtraction
+	 */
+	Vector3 sub(Vector3 a) {
+		return new Vector3(this.x - a.x, this.y - a.y, this.z - a.z);
+	}
+
+	/**
+	 * Vector scaling
+	 */
+	Vector3 mul(double a) {
+		return new Vector3(a * this.x, a * this.y, a * this.z);
+	}
+
+	/**
+	 * Dot Product
+	 */
+	double dot(Vector3 a) {
+		return this.x * a.x + this.y * a.y + this.z * a.z;
+	}
+
+	/**
+	 * Cross Product
+	 */
+	Vector3 cross(Vector3 a) {
+		return new Vector3(this.y * a.z - this.z * a.y, this.z * a.x - this.x * a.z, this.x * a.y - this.y * a.x);
+	}
+	
+	/**
+	 * Normalizes vector
+	 */
+	Vector3 norm() {
+		return this.mul(1d/Math.sqrt(this.dot(this)));
+	}
+}
